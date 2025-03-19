49665d82a3e08b45d83095131e19555c16c88c4d
NgocMy03 <myb2110019@student.ctu.edu.vn>
2025-03-19 10:42:25 +0700

    merge

--- resources/views/productindex.blade.php
+++ resources/views/productindex.blade.php
@@ -134,15 +134,25 @@
                     </div>
                 </div>
             </div>
+            {{-- <footer>
+                <div class="footer">
+                    <div class="text-center">
+                        <small> &copy;Copyright: CT298 - N01</small>
+                    </div>
+                </div>
+            </footer> --}}
+            {{--  --}}
+        </div>
+        <div class="footer">
+            <div class="pull-right">
+                10GB of <strong>250GB</strong> Free.
+            </div>
+            <div>
+                <strong>Copyright</strong> Example Company &copy; 2014-2017
+            </div>
         </div>
     </div>
-    {{-- <footer>
-    <div class="footer">
-        <div class="text-center">
-            <small> &copy;Copyright: CT298 - N01</small>
-        </div>
-    </div>
-</footer> --}}
+
 </body>
 
 </html>
